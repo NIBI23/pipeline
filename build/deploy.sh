5ce4ffaa5eff7b841d5d31b0e62dad2350971033
Ubuntu <ubuntu@ip-172-31-46-11.ap-south-1.compute.internal>
2024-03-20 13:34:34 +0000

    added

---
+++ build/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if the event is a push to the dev branch
+if [ "$GITHUB_EVENT_NAME" == "push" ] && [ "$GITHUB_REF" == "refs/heads/dev" ]; then
+    echo "Building and pushing Docker image to dev repository in Docker Hub..."
+    docker build -t app:dv1 .
+    docker tag app:v1 nibin23/dev:latest
+    docker login -u $dockerhubusername -p $dockerhubpassword
+    docker push nibin23/dev:latest
+
+# Check if the event is a merge from dev to master
+elif [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "$GITHUB_EVENT_ACTION" == "closed" ] && [ "$GITHUB_BASE_REF" == "dev" ] && [ "$GITHUB_HEAD_REF" == "master" ]; then
+    echo "Building and pushing Docker image to prod repository in Docker Hub..."
+    docker build -t app:pv1 .
+    docker tag app:pv1 nibin23/prod:latest
+    docker login -u $dockerhubusername -p $dockerhubpassword
+    docker push your-dockerhub-username/prod-repo:latest
+fi
